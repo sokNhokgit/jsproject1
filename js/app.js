71e69e8c5e1401e796458b1862a1ca7400f48a19
SOK.NHOK <sok.nhok@PNC.LAN>
2022-01-20 19:19:40 +0700

    newrepo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//1 create title for wrapper
+let wrapper = document.getElementById("wrapper")
+console.log(wrapper);
+let title = document.createElement("h1");
+title.className = "title";
+title.textContent = "All Task"
+wrapper.appendChild(title)
