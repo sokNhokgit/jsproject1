7b8f598e9b22eec8749c7091aba9b51ad350c0ad
SOK.NHOK <sok.nhok@PNC.LAN>
2022-01-20 19:39:48 +0700

    add title

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ let title = document.createElement("h1");
 title.className = "title";
 title.textContent = "All Task"
 wrapper.appendChild(title)
+// title 
+let titile = document.createElement("h1");
+title.className = "title";
+title.textContent = "hello title";
